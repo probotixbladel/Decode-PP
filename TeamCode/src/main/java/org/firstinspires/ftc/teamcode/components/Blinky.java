0585598b582196e2ed965f75f398b572dae9c282
notAnotI <keeslauwers@gmail.com>
2026-02-02 19:50:28 +0100

    blinky state machine and fix hot pink

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Blinky.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Blinky.java
@@ -15,71 +15,89 @@ public class Blinky {
 	public boolean strobeLawn = false;
     private boolean detecting = false;
 	private boolean wasDetecting = false;
+	public BlinkState state = BlinkState.IDLE;
+	public enum BlinkState {
+		WAS_DETECTING,
+		DETECTING,
+		IDLE
+	}
+
     public Blinky(HardwareMap hwm){
         this.hardwareMap = hwm;
         this.Blinky = hardwareMap.get(RevBlinkinLedDriver.class, "Blinkin");
         this.detectorTimer = new ElapsedTime();
+		this.strobeTimer = new ElapsedTime();
     }
 
     public void update(ComponentShell comps){
 		if (comps.detector.detecting) {
 			detectorTimer.reset();
 		}
-		detecting = detectorTimer.seconds() < detectorTimeTreshold;
-		wasDetecting = detectorTimer.seconds() + strobeTime < detectorTimeTreshold && !detecting;
+		if (detectorTimer.seconds() < detectorTimeTreshold) {
+			state = BlinkState.DETECTING;
+		}
 
-		if (wasDetecting) {
-			Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.HOT_PINK);
-		} else if (detecting) {
-			double relativeSpeed;
-			switch (comps.shooter.state) {
-				case LOW:
-					relativeSpeed = comps.shooter.CurrentVel / Shooter.MinSpeed;
-					if (relativeSpeed >= 1.0 - colorInterval) {
-						Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.YELLOW);
-					} else if (relativeSpeed >= 1.0 - colorInterval * 2) {
-						Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.GOLD);
-					} else if (relativeSpeed >= 1.0 - colorInterval * 3) {
-						Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.ORANGE);
-					} else if (relativeSpeed >= 1.0 - colorInterval * 4) {
-						Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED_ORANGE);
-					} else if (relativeSpeed >= 1.0 - colorInterval * 5) {
-						Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
-					} else {
-						Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.DARK_RED);
-					}
-					break;
-				case READY:
-					if (strobeTimer.seconds() > strobeTime) {
-						strobeTimer.reset();
-						strobeLawn = !strobeLawn;
-					}
+		switch (state) {
+			case WAS_DETECTING:
+				Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.HOT_PINK);
+				if (detectorTimer.seconds() > detectorTimeTreshold + strobeTime) {
+					state = BlinkState.IDLE;
+				}
+				break;
+			case DETECTING:
+				if (detectorTimer.seconds() > detectorTimeTreshold) {
+					state = BlinkState.WAS_DETECTING;
+				}
+				double relativeSpeed;
+				switch (comps.shooter.state) {
+					case LOW:
+						relativeSpeed = comps.shooter.CurrentVel / Shooter.MinSpeed;
+						if (relativeSpeed >= 1.0 - colorInterval) {
+							Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.YELLOW);
+						} else if (relativeSpeed >= 1.0 - colorInterval * 2) {
+							Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.GOLD);
+						} else if (relativeSpeed >= 1.0 - colorInterval * 3) {
+							Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.ORANGE);
+						} else if (relativeSpeed >= 1.0 - colorInterval * 4) {
+							Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED_ORANGE);
+						} else if (relativeSpeed >= 1.0 - colorInterval * 5) {
+							Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
+						} else {
+							Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.DARK_RED);
+						}
+						break;
+					case READY:
+						if (strobeTimer.seconds() > strobeTime) {
+							strobeTimer.reset();
+							strobeLawn = !strobeLawn;
+						}
 
-					if (strobeLawn) {
-						Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
-					} else {
-						Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.DARK_GREEN);
-					}
-					break;
-				case HIGH:
-					relativeSpeed = comps.shooter.CurrentVel / Shooter.MaxSpeed;
-					if (relativeSpeed <= 1.0 + colorInterval) {
-						Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.AQUA);
-					} else if (relativeSpeed <= 1.0 + colorInterval * 2) {
-						Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.SKY_BLUE);
-					} else if (relativeSpeed <= 1.0 + colorInterval * 3) {
-						Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.DARK_BLUE);
-					} else if (relativeSpeed <= 1.0 + colorInterval * 4) {
-						Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
-					} else if (relativeSpeed <= 1.0 + colorInterval * 5) {
-						Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE_VIOLET);
-					} else {
-						Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.VIOLET);
-					}
-					break;
-			}
-		} else {
-			Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.LAWN_GREEN);
+						if (strobeLawn) {
+							Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.LAWN_GREEN);
+						} else {
+							Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.WHITE);
+						}
+						break;
+					case HIGH:
+						relativeSpeed = comps.shooter.CurrentVel / Shooter.MaxSpeed;
+						if (relativeSpeed <= 1.0 + colorInterval) {
+							Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.AQUA);
+						} else if (relativeSpeed <= 1.0 + colorInterval * 2) {
+							Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.SKY_BLUE);
+						} else if (relativeSpeed <= 1.0 + colorInterval * 3) {
+							Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.DARK_BLUE);
+						} else if (relativeSpeed <= 1.0 + colorInterval * 4) {
+							Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+						} else if (relativeSpeed <= 1.0 + colorInterval * 5) {
+							Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE_VIOLET);
+						} else {
+							Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.VIOLET);
+						}
+						break;
+				}
+				break;
+			case IDLE:
+				Blinky.setPattern(RevBlinkinLedDriver.BlinkinPattern.LAWN_GREEN);
 		}
     }
 }
