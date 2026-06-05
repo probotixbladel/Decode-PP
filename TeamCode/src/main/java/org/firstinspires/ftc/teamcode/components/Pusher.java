2dd34752e04982037d13063036db463f7e08cea8
ProBotiX Bladel <probotixbladel@users.noreply.github.com>
2026-06-05 16:42:00 +0200

    made pusher higher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Pusher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Pusher.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.hardware.AnalogInput;
 public class Pusher {
 	public Servo pusher;
     public static double wait = 0.02;
-    public static double push = 0.35;
+    public static double push = 0.4;
     public ElapsedTime lastShot = new ElapsedTime();
 
     public PushState state = PushState.RETURNING;
