53307b365cc7489635438c6e4660acc3ee7e8d5e
ProBotiX Bladel <probotixbladel@users.noreply.github.com>
2026-06-11 16:38:53 +0200

    changed sensors and outputs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/ArtifactDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/ArtifactDetector.java
@@ -2,8 +2,9 @@ package org.firstinspires.ftc.teamcode.components;
 
 
 import com.bylazar.configurables.annotations.Configurable;
-import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
+//import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
@@ -14,7 +15,7 @@ public class ArtifactDetector {
 	public OpticalDistanceSensor firstArtifactSensor1;  // Hardware Device Object
 	public OpticalDistanceSensor firstArtifactSensor2;  // Hardware Device Object
     public ColorRangeSensor secondArtifactSensor;
-    public ModernRoboticsI2cColorSensor thirdArtifactSensor;
+    public RevColorSensorV3 thirdArtifactSensor;
     public ModernRoboticsI2cRangeSensor fourthArtifactSensor;
 	public double firstDistance = 0;
 	public double firstDistance2 = 0;
@@ -30,7 +31,7 @@ public class ArtifactDetector {
         //Distance sensor intake up - control hub i2c 2 RangeInUp
         //ColSens v2 - control hub i2c 0 ColSens
 		this.secondArtifactSensor = hwm.get(ColorRangeSensor.class, "ColThrough");
-        this.thirdArtifactSensor  = hwm.get(ModernRoboticsI2cColorSensor.class, "ColIn");
+        this.thirdArtifactSensor  = hwm.get(RevColorSensorV3.class, "ColIn");
         this.fourthArtifactSensor = hwm.get(ModernRoboticsI2cRangeSensor.class, "RangeIn");
 		this.firstArtifactSensor1 = hwm.get(OpticalDistanceSensor.class, "DistSens");
 		this.firstArtifactSensor2 = hwm.get(OpticalDistanceSensor.class, "DistSens2");
@@ -41,19 +42,23 @@ public class ArtifactDetector {
 		firstDistance2 = firstArtifactSensor2.getLightDetected();
         firstDetecting = (firstDistance > firstMinThreshold && firstDistance < firstMaxThreshold) || (firstDistance2 > firstMinThreshold2 && firstDistance2 < firstMaxThreshold2);
 		comps.telemetryM.debug(
-				"secondArtifact R G B A",
+				"secondArtifact: R, G, B, A, Distance",
 				secondArtifactSensor.red(),
 				secondArtifactSensor.green(),
 				secondArtifactSensor.blue(),
-                secondArtifactSensor.alpha()
+                secondArtifactSensor.alpha(),
+                secondArtifactSensor.getDistance(DistanceUnit.MM)
+
 		);
-		comps.telemetryM.debug("thirdArtifact R G B A",
+		comps.telemetryM.debug("thirdArtifact: R G B A, Distance, rawOptical",
 				thirdArtifactSensor.red(),
 				thirdArtifactSensor.green(),
 				thirdArtifactSensor.blue(),
-				thirdArtifactSensor.alpha()
+				thirdArtifactSensor.alpha(),
+                thirdArtifactSensor.getDistance(DistanceUnit.MM),
+                thirdArtifactSensor.rawOptical()
 		);
-		comps.telemetryM.debug("fourthArtifact, mm, raw ultrasonic, raw optical",
+		comps.telemetryM.debug("fourthArtifact: mm, raw ultrasonic, raw optical",
 				fourthArtifactSensor.getDistance(DistanceUnit.MM),
 				fourthArtifactSensor.rawUltrasonic(),
 				fourthArtifactSensor.rawOptical()
